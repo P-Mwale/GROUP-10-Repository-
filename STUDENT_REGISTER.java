295dd63bb105265f358468b61c3754cb8c6702fa
Euro-mountanio <129367034+Euro-mountanio@users.noreply.github.com>
2023-04-03 11:03:14 +0200

    Update STUDENT_REGISTER.java

--- STUDENT_REGISTER.java
+++ STUDENT_REGISTER.java
@@ -1,2 +1,23 @@
 
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class StudentRegister {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        ArrayList<Student> studentList = new ArrayList<>();
+
+        System.out.println("Welcome to the Student Register!");
+
+        while (true) {
+            System.out.println("Please select an option:");
+            System.out.println("1. Add Student");
+            System.out.println("2. View Student List");
+            System.out.println("3. Delete Student by Name");
+            System.out.println("4. Delete Student by ID");
+            System.out.println("5. Exit");
+
+            int choice = input.nextInt();
+            input.nextLine(); // consume newline character
+
+            switch (choice) {
