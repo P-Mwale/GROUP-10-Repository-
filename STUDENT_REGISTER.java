a5ab1a17f180fe75ba53560e9a5d0c72cff25bbb
P-Mwale <129364654+P-Mwale@users.noreply.github.com>
2023-04-03 02:06:42 -0700

    Update STUDENT_REGISTER.java

--- STUDENT_REGISTER.java
+++ STUDENT_REGISTER.java
@@ -21,3 +21,22 @@ public class StudentRegister {
             input.nextLine(); // consume newline character
 
             switch (choice) {
+                case 1:
+                    System.out.println("Enter student name:");
+                    String name = input.nextLine();
+
+                    System.out.println("Enter student ID:");
+                    String id = input.nextLine();
+
+                    System.out.println("Enter student major:");
+                    String major = input.nextLine();
+
+                    Student student = new Student(name, id, major);
+                    studentList.add(student);
+
+                    System.out.println("Student added successfully.");
+                    System.out.println(" ");
+                    break; 
+
+
+
