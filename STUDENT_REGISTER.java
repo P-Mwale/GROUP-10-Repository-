ad1280926b7620009ac09a36a6b476da24d3bdaf
Euro-mountanio <129367034+Euro-mountanio@users.noreply.github.com>
2023-04-05 12:16:56 +0200

    Update STUDENT_REGISTER.java

--- STUDENT_REGISTER.java
+++ STUDENT_REGISTER.java
@@ -90,6 +90,32 @@ public class StudentRegister {
         }
         return false;
     }
+}    
+    class Student {
+        private String name;
+        private String id;
+        private String major;
+
+        public Student(String name, String id, String major) {
+            this.name = name;
+            this.id = id;
+            this.major = major;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public String getMajor() {
+            return major;
+        }
+}
+
+    
 
 
 
